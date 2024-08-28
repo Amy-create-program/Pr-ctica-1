93a3e46ed42f85775dd8a8dc8ee975bcd8921491
Aimee <aimee.gutierrez@ucsp.edu.pe>
2024-08-28 18:15:18 -0500

    Update ejercicio2.27.cpp

---
+++ Practica de Laboratorio 1/ejercicio2.27.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+    char inputCharacter; 
+
+    std::cout << "Ingrese un caracter: ";
+    std::cin >> inputCharacter; 
+
+    std::cout << "El equivalente estero de '" << inputCharacter << "' es: ";
+    std::cout << static_cast<int>(inputCharacter) << std::endl;
+
+    return 0;
+}
