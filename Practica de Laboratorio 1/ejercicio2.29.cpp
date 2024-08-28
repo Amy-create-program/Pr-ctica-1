4c38f8be05d6493ccb63c595aac0d2a04b48b099
Aimee <aimee.gutierrez@ucsp.edu.pe>
2024-08-28 18:12:23 -0500

    Update ejercicio2.29.cpp

--- Practica de Laboratorio 1/ejercicio2.29.cpp
+++ Practica de Laboratorio 1/ejercicio2.29.cpp
@@ -1,2 +1,16 @@
+#include <iostream>
 
+int main() {
+    std::cout << "Face length \tSurface area \tVolume" << std::endl;
+    std::cout << "of cube (cm)\tof cube (cm^2)\tof cube(cm^3)" << std::endl;
+
+    for (int faceLength = 0; faceLength <= 4; ++faceLength) {
+        double surfaceArea = 6 * faceLength * faceLength; // calculate surface area
+        double volume = faceLength * faceLength * faceLength; // calculate volume
+
+        std::cout << faceLength << "\t\t" << surfaceArea << "\t\t" << volume << std::endl;
+    }
+
+    return 0;
+}
 
