ab9736826770e56ddec4d6b27435a8ce15c331a9
Aimee <aimee.gutierrez@ucsp.edu.pe>
2024-08-28 20:17:28 -0500

    Update ejercicio2.26.cpp

---
+++ Practica de Laboratorio 1/ejercicio2.26.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "* * * * * * * *\n";
+    std::cout << " * * * * * * * *\n";
+    std::cout << "* * * * * * * *\n";
+    std::cout << " * * * * * * * *\n";
+    std::cout << "* * * * * * * *\n";
+    std::cout << " * * * * * * * *\n";
+    std::cout << "* * * * * * * *\n";
+    std::cout << " * * * * * * * *\n";
+
+    return 0;
+}
