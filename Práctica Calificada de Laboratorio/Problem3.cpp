c4807ad10cde7c7c0171f8e4767d5d6412a1c2b8
Aimee <aimee.gutierrez@ucsp.edu.pe>
2024-09-17 19:47:59 -0500

    Update Problem3.cpp

--- Práctica Calificada de Laboratorio/Problem3.cpp
+++ Práctica Calificada de Laboratorio/Problem3.cpp
@@ -2,32 +2,33 @@
 using namespace std; 
 
 bool esPrimo(int num) { //Variables booleanas solo pueden contener verdadero y falso.
+    int i=2;
     if (num <= 1){
         return false;
     }
-    for (int i = 2; i * i <= num; i++){
-        if (num % i == 0){
+    while (i < num){
+        if (num % i == 0) {
             return false;
         }
+        i++;
     }
-    return true;
+    return true; 
+}   
+void imprimirPrimos(int n) {
+    for (int i {2}; i < n; i++){
+        if (esPrimo(i)){
+            cout << i << " ";
+        }
+    }
 }
 
 int main(){
     int n;
-    cout << "Ingrese un número: ";
+    cout << "Ingrese un número: "; 
     cin >> n;
 
-    cout << "Los números primos menos a " << n << " son: ";
-    for (int i = 2; i < n; i++) {
-        if (esPrimo(i)) {
-            cout << i;
-            if (i < n - 1) {
-                cout << ", ";
-            }
-        }
-    }
-    cout << endl;
+    imprimirPrimos(n);
 
     return 0;
+
 }
